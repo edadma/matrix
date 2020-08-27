5feef65e020c0c477857b952140b9e55951a9042
Edward A Maxedon, Sr <edadma@vinctus.com>
2020-08-27 08:57:17 -0400

    code view()

--- src/main/scala/xyz/hyperreal/matrix/Matrix.scala
+++ src/main/scala/xyz/hyperreal/matrix/Matrix.scala
@@ -1,14 +1,32 @@
 package xyz.hyperreal.matrix
 
 import scala.collection.immutable.{ArraySeq, IndexedSeq}
-
 import xyz.hyperreal.table.TextTable
 
-class Matrix(val rows: Int, val cols: Int, init: (Int, Int) => Number)
-    extends IndexedSeq[Number]
-    with ((Int, Int) => Number) {
+import scala.collection.mutable.ArrayBuffer
 
-  private val data = ArraySeq.tabulate[Number](rows, cols)((x: Int, y: Int) => init(x + 1, y + 1))
+abstract class Matrix extends IndexedSeq[Number] with ((Int, Int) => Number) {
+
+  val rows: Int
+  val cols: Int
+
+  def length: Int = rows * cols
+
+  def row(r: Int) = new ConcreteMatrix(1, cols, (_, c) => apply(r, c))
+
+  def col(c: Int) = new ConcreteMatrix(rows, 1, (r, _) => apply(r, c))
+
+  override def toString: String =
+    new TextTable() {
+      for (i <- 1 to rows)
+        rowSeq(Matrix.this.row(i))
+    }.toString
+
+}
+
+class ConcreteMatrix(val rows: Int, val cols: Int, init: (Int, Int) => Number) extends Matrix {
+
+  private val data = ArraySeq.tabulate[Number](rows, cols)((i: Int, j: Int) => init(i + 1, j + 1))
 
   def apply(row: Int, col: Int): Number = {
     require(0 < row && row <= rows, s"new Matrix: row out of range: $row")
@@ -16,17 +34,28 @@ class Matrix(val rows: Int, val cols: Int, init: (Int, Int) => Number)
     data(row - 1)(col - 1)
   }
 
+  def view(ridx: Int, height: Int, cidx: Int, width: Int): Matrix = {
+    require(1 <= ridx && ridx <= rows, s"Matrix.view: row out of range: $ridx")
+    require(1 <= cidx && cidx <= rows, s"Matrix.view: col out of range: $cidx")
+    require(1 <= height && height <= rows, s"Matrix.view: height out of range: $height")
+    require(1 <= width && width <= cols, s"Matrix.view: width out of range: $width")
+    new Matrix {
+      val rows: Int = width
+      val cols: Int = height
+
+      def apply(idx: Int): Number = apply(idx / cols + 1, idx % cols + 1)
+
+      def apply(r: Int, c: Int): Number = ConcreteMatrix.this(r + ridx - 1, c + cidx - 1)
+    }
+  }
+
   def apply(idx: Int): Number = data(idx / cols)(idx % cols)
 
-  val length: Int = rows * cols
-
-  def row(r: Int) = new Matrix(1, cols, (_, c) => apply(r, c))
-
-  def col(c: Int) = new Matrix(rows, 1, (r, _) => apply(r, c))
+  val dim: (Int, Int) = (rows, cols)
 
   def operation(that: Matrix, name: String, op: (Number, Number) => Number): Matrix = {
     require(rows == that.rows && cols == that.cols, s"$name: operand matrices must be of equal dimension")
-    new Matrix(rows, cols, (i, j) => op(this(i, j), that(i, j)))
+    new ConcreteMatrix(rows, cols, (i, j) => op(this(i, j), that(i, j)))
   }
 
   def add(that: Matrix): Matrix = operation(that, "Matrix.add", _.doubleValue + _.doubleValue)
@@ -48,17 +77,11 @@ class Matrix(val rows: Int, val cols: Int, init: (Int, Int) => Number)
 
   def mul(that: Matrix): Matrix = {
     require(cols == that.rows, "Matrix.mul: width of left operand must equal height of right operand")
-    new Matrix(rows, that.cols, (i, j) => this.row(i) prod that.col(j))
+    new ConcreteMatrix(rows, that.cols, (i, j) => this.row(i) prod that.col(j))
   }
 
   def *(that: Matrix): Matrix = mul(that)
 
-  override def toString: String =
-    new TextTable() {
-      data foreach { r =>
-        rowSeq(r)
-      }
-    }.toString
 }
 
 object Matrix {
@@ -67,10 +90,10 @@ object Matrix {
     require(data.nonEmpty, "Matrix cannot be empty")
     require(data forall (_.nonEmpty), "Matrix cannot have an empty row")
     require(data forall (_.length == data.head.length), "Matrix must have rows of same length")
-    new Matrix(data.length, data.head.length, (x: Int, y: Int) => data(x - 1)(y - 1))
+    new ConcreteMatrix(data.length, data.head.length, (x: Int, y: Int) => data(x - 1)(y - 1))
   }
 
-  def diagonal(size: Int, value: Number) = new Matrix(size, size, (i, j) => if (i == j) value else 0)
+  def diagonal(size: Int, value: Number) = new ConcreteMatrix(size, size, (i, j) => if (i == j) value else 0)
 
   def identity(size: Int): Matrix = diagonal(size, 1)
 
@@ -81,7 +104,24 @@ object Matrix {
 
   def col(elems: Number*): Matrix = {
     require(elems.nonEmpty, "Matrix.col: need at least one element")
-    new Matrix(elems.length, 1, (r, _) => elems(r))
+    new ConcreteMatrix(elems.length, 1, (r, _) => elems(r - 1))
+  }
+
+  def cath(mat: Matrix, mats: Matrix*): Matrix = {
+    require(mats.forall(m => m.rows == mat.rows),
+            "Matrix.cath: matrices being horizontally concatenated must have same height")
+
+    val ms = new ArrayBuffer[(Matrix, Int)]
+    var rcols = 0
+
+    for (m <- mat +: mats) {
+      for (i <- 1 to m.cols)
+        ms append ((m, i))
+
+      rcols += m.cols
+    }
+
+    new ConcreteMatrix(mat.rows, rcols, (i, j) => ms(j - 1)._1(i, ms(j - 1)._2))
   }
 
 }
--- src/test/scala/xyz/hyperreal/matrix/Main.scala
+++ src/test/scala/xyz/hyperreal/matrix/Main.scala
@@ -4,7 +4,9 @@ object Main extends App {
 
   val a = Matrix(List(List(1, 2, 3), List(4, 5, 6), List(7, 8, 9)))
   val b = Matrix(List(List(10, 20, 30), List(4, 5, 6)))
+  val c = Matrix.col(11, 12, 13)
+  val d = Matrix.row(14, 15, 16)
 
-  println(a * Matrix.diagonal(3, 2))
+  println(Matrix.cath(c, a))
 
 }
