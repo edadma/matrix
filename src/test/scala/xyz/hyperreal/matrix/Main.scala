657dcc2b638e2f0cd92857685c6c9fe69f774fb1
Edward A Maxedon, Sr <edadma@vinctus.com>
2020-08-28 12:23:42 -0400

    better example

--- src/test/scala/xyz/hyperreal/matrix/Main.scala
+++ src/test/scala/xyz/hyperreal/matrix/Main.scala
@@ -4,10 +4,10 @@ import xyz.hyperreal.numbers_jvm.Rational
 
 object Main extends App {
 
-  val a = Matrix[Rational](List(List(-2, 2, -3), List(-1, 1, 3), List(2, 0, -1)))
-  val b = Matrix[Rational](List(List(1, -4), List(0, 3)))
+  val a = Matrix[Rational](List(List(3, 2, -5), List(1, -3, 2), List(5, -1, 4)))
+  val b = Matrix.col[Rational](12, -13, 10)
 
-  println(a.det)
-  println(a.inv * a)
+  println(a.inv)
+  println(a.inv * b)
 
 }
