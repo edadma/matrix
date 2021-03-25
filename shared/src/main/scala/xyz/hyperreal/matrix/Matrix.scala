9cc052267d185da34ba1dcd893f8f045cb273018
Edward A Maxedon, Sr <edadma@vinctus.com>
2021-03-25 10:06:48 -0400

    fix method names

--- shared/src/main/scala/xyz/hyperreal/matrix/Matrix.scala
+++ shared/src/main/scala/xyz/hyperreal/matrix/Matrix.scala
@@ -181,45 +181,23 @@ abstract class Matrix[F](implicit classTag: ClassTag[F], field: Fractional[F])
 
   def replaceRow(v: Matrix[F], idx: Int): Matrix[F] = {
     require(v.isRow, "can only replace a row vector")
+    require(1 <= idx && idx <= rows, s"row index out of range: $idx")
 
-    if (v.isColumn) {
-      require(1 <= idx && idx <= cols, s"column index out of range: $idx")
-
-      idx match {
-        case 1      => Matrix.cath(v, removeCol(1))
-        case `cols` => Matrix.cath(removeCol(cols), v)
-        case _      => Matrix.cath(block(1, rows, 1, idx - 1), v, block(1, rows, idx + 1, cols - idx))
-      }
-    } else {
-      require(1 <= idx && idx <= rows, s"row index out of range: $idx")
-
-      idx match {
-        case 1      => Matrix.catv(v, removeRow(1))
-        case `rows` => Matrix.catv(removeRow(cols), v)
-        case _      => Matrix.catv(block(1, idx - 1, 1, cols), v, block(idx + 1, rows - idx, 1, cols))
-      }
+    idx match {
+      case 1      => Matrix.catv(v, removeRow(1))
+      case `rows` => Matrix.catv(removeRow(cols), v)
+      case _      => Matrix.catv(block(1, idx - 1, 1, cols), v, block(idx + 1, rows - idx, 1, cols))
     }
   }
 
-  def replace(v: Matrix[F], idx: Int): Matrix[F] = {
-    require(v.isVector, "can only replace a row or column vector")
+  def replaceCol(v: Matrix[F], idx: Int): Matrix[F] = {
+    require(v.isColumn, "can only replace a column vector")
+    require(1 <= idx && idx <= cols, s"column index out of range: $idx")
 
-    if (v.isColumn) {
-      require(1 <= idx && idx <= cols, s"column index out of range: $idx")
-
-      idx match {
-        case 1      => Matrix.cath(v, removeCol(1))
-        case `cols` => Matrix.cath(removeCol(cols), v)
-        case _      => Matrix.cath(block(1, rows, 1, idx - 1), v, block(1, rows, idx + 1, cols - idx))
-      }
-    } else {
-      require(1 <= idx && idx <= rows, s"row index out of range: $idx")
-
-      idx match {
-        case 1      => Matrix.catv(v, removeRow(1))
-        case `rows` => Matrix.catv(removeRow(cols), v)
-        case _      => Matrix.catv(block(1, idx - 1, 1, cols), v, block(idx + 1, rows - idx, 1, cols))
-      }
+    idx match {
+      case 1      => Matrix.cath(v, removeCol(1))
+      case `cols` => Matrix.cath(removeCol(cols), v)
+      case _      => Matrix.cath(block(1, rows, 1, idx - 1), v, block(1, rows, idx + 1, cols - idx))
     }
   }
 
